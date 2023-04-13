1ef07a45422d23fb32a0b3b35dacb1405295fea9
Kamado Yuta <ivankianica356@gmail.com>
2023-04-13 20:47:08 +0300

    feature: log rules

--- js/sing_in.js
+++ js/sing_in.js
@@ -7,15 +7,29 @@ const SecondNAME = document.getElementById("exampleInputSecondName1");
 // mail and password const
 const MAIL = document.getElementById("exampleInputEmail1");
 const PASSWORD = document.getElementById("exampleInputPassword1");
+let mail_check = "@gmail.com";
 
 LOG_BUTTON.addEventListener("click", () => {
   // first part
-  console.log(MAIL.value);
-  console.log(PASSWORD.value);
+  if (NAME.value.length < 4 || NAME.value === undefined) {
+    alert("Будь ласка введіть довше ім'я");
+  } else if (SecondNAME.value.length < 4 || SecondNAME.value === undefined) {
+    alert("Будь ласка введіть довше прізвище");
+  } else if (
+    PASSWORD.value.length < 7 ||
+    PASSWORD.value === undefined ||
+    PASSWORD.value.length == 7
+  ) {
+    alert(
+      "Будь ласка введіть правильний пароль довжина якого має бути 8, або більше"
+    );
+  }
+
   // second part
-  alert(NAME.value + " " + SecondNAME.value + " ," + " раді тебе бачити!");
+  else {
+    alert(NAME.value + " " + SecondNAME.value + " ," + " раді тебе бачити!");
+    console.log("Email: " + MAIL.value + "; " + "Password:" + PASSWORD.value);
+  }
 });
 
-// // need maybe
-// console.log(MAIL);
-// console.log(PASSWORD);
+// console.log("Email: " + MAIL.value + "; " + "Password:" + PASSWORD.value);
